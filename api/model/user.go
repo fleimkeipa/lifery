1a6ee8e6d6ee7b4b7adf60685391d5a34ebfdf52
fleimkeipa <adem_sahin12@hotmail.com>
2024-11-24 02:14:17 +0300

    Add connects field on user struct

--- api/model/user.go
+++ api/model/user.go
@@ -7,9 +7,10 @@ const ZeroCreds = "zeroCreds"
 type User struct {
 	DeletedAt time.Time `json:"deleted_at" pg:",soft_delete"`
 	CreatedAt time.Time `json:"created_at"`
-	Username  string    `json:"username" binding:"required" pg:",unique"`
-	Email     string    `json:"email" binding:"required" pg:",unique"`
-	Password  string    `json:"password" binding:"required"`
+	Connects  []int     `json:"connects"`
+	Username  string    `json:"username" pg:",unique"`
+	Email     string    `json:"email" pg:",unique"`
+	Password  string    `json:"password"`
 	ID        int64     `json:"id" pg:",pk"`
 	RoleID    uint      `json:"role_id"`
 }
@@ -18,13 +19,6 @@ type UserList struct {
 	Users []User `json:"users"`
 	Total int    `json:"total"`
 	PaginationOpts
-}
-
-type Owner struct {
-	Username string `json:"username" pg:",unique"`
-	Email    string `json:"email" pg:",unique"`
-	ID       int64  `json:"id" pg:",pk"`
-	RoleID   uint   `json:"role_id"`
 }
 
 type UserCreateRequest struct {
