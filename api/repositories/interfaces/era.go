70f669bc8aae6b82482de7d65b03e2e182254941
fleimkeipa <adem_sahin12@hotmail.com>
2024-11-25 07:53:12 +0300

    Fix typos

--- api/repositories/interfaces/era.go
+++ api/repositories/interfaces/era.go
@@ -7,8 +7,8 @@ import (
 )
 
 type EraRepository interface {
-	Create(ctx context.Context, event *model.Era) (*model.Era, error)
-	Update(ctx context.Context, eraID string, event *model.Era) (*model.Era, error)
+	Create(ctx context.Context, era *model.Era) (*model.Era, error)
+	Update(ctx context.Context, eraID string, era *model.Era) (*model.Era, error)
 	Delete(ctx context.Context, eraID string) error
 	List(ctx context.Context, opts *model.EraFindOpts) (*model.EraList, error)
 	GetByID(ctx context.Context, eraID string) (*model.Era, error)
