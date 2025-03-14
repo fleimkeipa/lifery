85b92f82e7cf908d9dd47368a16b5dc983101663
fleimkeipa <adem_sahin12@hotmail.com>
2025-03-15 00:11:54 +0300

    Update errors

--- api/controller/errors.go
+++ api/controller/errors.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/fleimkeipa/lifery/pkg"
 
@@ -14,14 +16,33 @@ func HandleEchoError(c echo.Context, err error) error {
 	var pe *pkg.Error
 
 	if errors.As(err, &pe) {
+		message := pe.Message()
+		errorMessage := func() string {
+			errMessage := pe.Error()
+			if errMessage == "" {
+				return ""
+			}
+			if strings.HasPrefix(errMessage, "ERROR:") {
+				return errMessage
+			}
+			return fmt.Sprintf("error: %s", errMessage)
+		}()
+
+		if message != "" && errorMessage != "" {
+			message = fmt.Sprintf("%s | %s", message, errorMessage)
+		}
+		if message == "" {
+			message = errorMessage
+		}
+
 		return c.JSON(pe.StatusCode(), FailureResponse{
-			Error:   pe.Error(),
-			Message: pe.Message(),
-		})
-	} else {
-		return c.JSON(http.StatusInternalServerError, FailureResponse{
 			Error:   err.Error(),
-			Message: "Internal Server Error",
+			Message: message,
 		})
 	}
+
+	return c.JSON(http.StatusInternalServerError, FailureResponse{
+		Error:   err.Error(),
+		Message: "Internal Server Error",
+	})
 }
