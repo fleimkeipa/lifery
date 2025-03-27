775f7bb5fc226fc3a11c14bbab156a83e3cfdbb9
fleimkeipa <adem_sahin12@hotmail.com>
2025-03-27 05:03:51 +0300

    Update jwt decode controls

--- ui/middleware/auth.ts
+++ ui/middleware/auth.ts
@@ -1,5 +1,27 @@
+import jwtDecode from "jwt-decode";
+
 export default defineNuxtRouteMiddleware((to, from) => {
-  const isAuthenticated = () => process.client ? localStorage.getItem('auth_token') : null;
+  const isAuthenticated = () => {
+    if (!process.client) {
+      return null;
+    }
+
+    const token = localStorage.getItem("auth_token");
+    if (!token) return null;
+
+    try {
+      const decoded: { eat: number } = jwtDecode(token);
+      if (decoded.eat * 1000 < Date.now()) {
+        localStorage.removeItem("auth_token");
+        return null;
+      }
+      return token;
+    } catch (error) {
+      localStorage.removeItem("auth_token");
+      return null;
+    }
+  };
+
   if (!isAuthenticated()) {
     return navigateTo("/login");
   }
