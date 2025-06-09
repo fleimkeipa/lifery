59f39de889a2936b268cb1331f5cfe39d77938df
sems <yilmazsems@gmail.com>
2025-06-10 00:29:59 +0300

    feat: logout

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -3,9 +3,19 @@
     <div class="w-2/3">
       <div class="flex justify-between items-center py-4">
         <navigation-menu />
-        <LanguageSwitcher />
+        <div class="flex items-center gap-2">
+          <LanguageSwitcher />
+          <UButton color="red" variant="ghost" @click="logout">{{ $t('common.logout') }}</UButton>
+        </div>
       </div>
       <slot class="mt-4" />
     </div>
   </div>
 </template>
+
+<script setup>
+const logout = () => {
+  localStorage.removeItem('auth_token')
+  window.location.href = '/login'
+}
+</script>
