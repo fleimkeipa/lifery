689738f7df976774f1044e78c2ac3f2c7b5e1f69
fleimkeipa <adem_sahin12@hotmail.com>
2025-06-09 04:34:16 +0300

    Delete redundant connect control on userUC

--- api/uc/user.go
+++ api/uc/user.go
@@ -3,7 +3,6 @@ package uc
 import (
 	"context"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/fleimkeipa/lifery/model"
@@ -86,64 +85,6 @@ func (rc *UserUC) Update(ctx context.Context, userID string, req model.UserCreat
 	}
 
 	return updatedUser, nil
-}
-
-func (rc *UserUC) UpdateConnects(ctx context.Context, user *model.User, senderID, receiverID string) error {
-	sID, err := strconv.Atoi(senderID)
-	if err != nil {
-		return pkg.NewError(err, "failed to convert senderID to int", http.StatusInternalServerError)
-	}
-
-	user.Connects = append(user.Connects, &model.Connect{
-		ID:       strconv.Itoa(sID),
-		Status:   model.RequestStatusPending,
-		UserID:   senderID,
-		FriendID: receiverID,
-	})
-
-	_, err = rc.userRepo.Update(ctx, receiverID, user)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (rc *UserUC) DeleteUserConnect(ctx context.Context, user *model.User, userID string) error {
-	for i, v := range user.Connects {
-		if v.ID == userID {
-			user.Connects = append(user.Connects[:i], user.Connects[i+1:]...)
-			break
-		}
-	}
-
-	_, err := rc.userRepo.Update(ctx, userID, user)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (rc *UserUC) IsConnected(ctx context.Context, userID, friendID string) (bool, error) {
-	// receiver exist control
-	receiver, err := rc.GetByID(ctx, userID)
-	if err != nil {
-		return false, err
-	}
-
-	connects := receiver.Connects
-	if len(connects) == 0 {
-		return false, nil
-	}
-
-	for _, v := range connects {
-		if v.ID == friendID {
-			return true, nil
-		}
-	}
-
-	return false, nil
 }
 
 func (rc *UserUC) List(ctx context.Context, opts *model.UserFindOpts) (*model.UserList, error) {
