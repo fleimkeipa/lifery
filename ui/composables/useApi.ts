5a9d601c82f0ba727cf3841ec05dc7624cb271ef
sems <yilmazsems@gmail.com>
2025-06-10 20:36:50 +0300

    fix: useApi

--- ui/composables/useApi.ts
+++ ui/composables/useApi.ts
@@ -1,24 +1,29 @@
 import { createFetch } from "@vueuse/core";
 
-
-export default createFetch({
-  baseUrl: process.env.NUXT_API_BASE_URL || "http://localhost:8080",
-  options: {
-    beforeFetch({ options }) {
-      const token = typeof window !== 'undefined' ? localStorage.getItem('auth_token') : null;
-      if (token) {
-        options.headers = {
-          ...options.headers,
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        };
-      } else {
-        options.headers = {
-          ...options.headers,
-          'Content-Type': 'application/json',
-        };
-      }
-      return { options };
+const useApi = (...args: any[]) => {
+  const api = createFetch({
+    baseUrl: useRuntimeConfig().public.apiBase,
+    options: {
+      beforeFetch({ options }) {
+        const token = typeof window !== 'undefined' ? localStorage.getItem('auth_token') : null;
+        if (token) {
+          options.headers = {
+            ...options.headers,
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json',
+          };
+        } else {
+          options.headers = {
+            ...options.headers,
+            'Content-Type': 'application/json',
+          };
+        }
+        return { options };
+      },
     },
-  },
-});
+  });
+
+  return api(...args)
+}
+
+export default useApi
