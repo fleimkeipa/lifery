9f38f72b2ca39ae0984fc186db38b69875e31eb6
fleimkeipa <adem_sahin12@hotmail.com>
2025-06-20 01:00:05 +0300

    Add password show hide button

--- ui/pages/login/index.vue
+++ ui/pages/login/index.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { object, string, type InferType } from "yup";
+import { ref, reactive } from 'vue'
 import type { FormSubmitEvent } from "@nuxt/ui";
 import { useI18n } from "vue-i18n";
+
 definePageMeta({
   layout: "not-authenticated",
   middleware: "no-auth"
@@ -11,11 +13,12 @@ const { t, locale } = useI18n();
 
 const schema = object({
   username: string().required(t('login.validation.required.username')),
-  password: string()
-    .required(t('login.validation.required.password')),
+  password: string().required(t('login.validation.required.password')),
 });
 
 type Schema = InferType<typeof schema>;
+
+const show = ref(false);
 
 const state = reactive({
   username: undefined,
@@ -42,11 +45,8 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
   }
 
   if (data.value) {
-    // Store the token in localStorage
     localStorage.setItem('auth_token', data.value.token);
     localStorage.setItem('username', data.value.username);
-
-    // Navigate to the events page
     await navigateTo('/home');
   }
 }
@@ -63,11 +63,20 @@ async function onSubmit(event: FormSubmitEvent<Schema>) {
         </UFormGroup>
 
         <UFormGroup :label="t('login.password')" name="password">
-          <UInput v-model="state.password" type="password" />
+          <UInput v-model="state.password" placeholder="Password" :type="show ? 'text' : 'password'">
+            <template #trailing>
+              <UButton class="pointer-events-auto" color="gray" variant="link"
+                :icon="show ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                :aria-label="show ? 'Hide password' : 'Show password'" :aria-pressed="show" aria-controls="password"
+                @click.prevent="show = !show" />
+            </template>
+          </UInput>
         </UFormGroup>
 
         <div class="flex w-full justify-center">
-          <UButton type="submit" class="mt-4" block> {{ t('login.submit') }} </UButton>
+          <UButton type="submit" class="mt-4" block>
+            {{ t('login.submit') }}
+          </UButton>
         </div>
 
         <div class="flex w-full justify-center mt-4">
