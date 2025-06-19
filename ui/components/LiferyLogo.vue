860f1034da4e31852f3533158829181373c58a1e
fleimkeipa <adem_sahin12@hotmail.com>
2025-06-19 18:09:01 +0300

    Update lifery logo nagivation

--- ui/components/LiferyLogo.vue
+++ ui/components/LiferyLogo.vue
@@ -1,24 +1,12 @@
 <template>
-  <span
-    class="font-extrabold text-2xl cursor-pointer select-none text-blue-700 dark:text-blue-300"
-    @click="goHome"
-  >
+  <span class="font-extrabold text-2xl cursor-pointer select-none text-blue-700 dark:text-blue-300" @click="goHome">
     Lifery
   </span>
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-const router = useRouter();
 
 function goHome() {
-  if (process.client) {
-    const token = localStorage.getItem('auth_token');
-    if (token) {
-      router.push('/');
-    } else {
-      router.push('/intro');
-    }
-  }
+  navigateTo('/');
 }
-</script> 
+</script>
