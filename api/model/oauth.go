abe3b14ecfc0fa3a74424d28574204554a6b4f7b
fleimkeipa <adem_sahin12@hotmail.com>
2025-06-21 19:44:57 +0300

    Add oauth models

---
+++ api/model/oauth.go
@@ -0,0 +1,16 @@
+package model
+
+type GoogleAuthRequest struct {
+	Code string `json:"code" validate:"required"`
+}
+
+type GoogleUserInfo struct {
+	ID            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_email"`
+	Name          string `json:"name"`
+	GivenName     string `json:"given_name"`
+	FamilyName    string `json:"family_name"`
+	Picture       string `json:"picture"`
+	Locale        string `json:"locale"`
+}
